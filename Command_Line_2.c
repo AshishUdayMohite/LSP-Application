2219f2c83e38a5016b3082ce54f377cd0a069067
Ashish Mohite <ashishmohite541@gmail.com>
2025-12-20 23:24:27 +0530

    command line argument

---
+++ Command_Line_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char * argv[])
+{
+    printf("Number of Command  linke Arks : %d \n",argc);
+
+    printf("Argv of 0 : %s \n",argv[0]);
+    printf("Argv of 0 : %s \n",argv[1]);
+    printf("Argv of 0 : %s \n",argv[2]);
+
+    return 0;
+}
