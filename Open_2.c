7d0300a311657d3bc3c86a22673b8b892c5d1a62
Ashish Mohite <ashishmohite541@gmail.com>
2025-12-20 20:39:48 +0530

    Open System call where file is Not Present

---
+++ Open_2.c
@@ -0,0 +1,20 @@
+#include<fcntl.h>
+#include<stdio.h>
+
+int main()
+{
+    int fd = 0;
+
+    fd = open("Second.c",O_RDWR);
+
+    if(fd==-1)
+    {
+        printf("Unable to Open File\n");
+    }
+    else
+    {
+        printf("File gets successful open with fd : %d\n",fd);
+    }
+
+    return 0;
+}
